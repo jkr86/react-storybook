6dcd3f46b92fcba2e2eac83e86dad25285d5dc66
Junaid E. Rajput <johnyrj97@gmail.com>
2022-01-05 13:38:43 +0500

    initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CustomSearch from "./components/custom-search";
+
+function App() {
+  return (
+    <div className="App">
+      <CustomSearch />
+    </div>
+  );
+}
+
+export default App;
