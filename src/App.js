f7aa586c80d90cb531fa98bd4ceac3edfb3a3f2d
Junaid E. Rajput <johnyrj97@gmail.com>
2022-01-05 19:31:36 +0500

    logic complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from "./logo.svg";
+import HomeBg from "./assets/search/Desktop.png";
 import "./App.css";
 import CustomSearch from "./components/custom-search";
 
 function App() {
   return (
-    <div className="App">
-      <CustomSearch />
+    <div className="h-screen w-full relative">
+      <img src={HomeBg} className="absolute brightness-75 inset-0 w-full h-full object-cover object-top z-0" alt="decorative background" />
+      <div className="w-full flex justify-end relative z-10">
+        <CustomSearch />
+      </div>
     </div>
   );
 }
